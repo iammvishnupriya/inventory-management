69bd1e8b1cbc9a293f6643927193a2073088b664
kalpana653 <139218321+kalpana653@users.noreply.github.com>
2025-04-24 12:30:31 +0530

    Delete Inventory Management/Inventory-Management directory

--- Inventory Management/Inventory-Management/src/main/java/com/erp/Inventory/Management/config/TokenValidationException.java
+++
@@ -1,9 +0,0 @@
-package com.erp.Inventory.Management.config;
-
-public class TokenValidationException extends RuntimeException {
-
-    public TokenValidationException(String message) {
-        super(message);  // Pass the message to the parent (RuntimeException)
-    }
-}
-
